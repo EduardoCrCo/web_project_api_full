e43b0318c66d972c43aec54cddc59034f73c78ba
EduardoCrCo <ecc.informatik@icloud.com>
2025-10-01 15:04:22 +0000

    add change api URL on login

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -58,7 +58,7 @@ function App() {
   useEffect(() => {
     const token = localStorage.getItem("jwt");
     if (token) {
-      fetch("https://api.web-project-around.ignorelist.com//users/me", {
+      fetch("https://api.web-project-around.ignorelist.com/users/me", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
@@ -110,7 +110,7 @@ function App() {
   const handleLogin = () => {
     setIsLoggedIn(true);
     const token = localStorage.getItem("jwt");
-    fetch("https://se-register-api.en.tripleten-services.com/v1/users/me", {
+    fetch("https://api.web-project-around.ignorelist.com/users/me", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
