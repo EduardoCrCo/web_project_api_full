78b61d5d60c5af4c434cda0bd1ece8806a3b36d2
EduardoCrCo <ecc.informatik@icloud.com>
2025-10-01 05:08:00 +0000

    Add CORS support and fix middlewares order

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import express from 'express'
-
 import mongoose from 'mongoose'
+import cors from "cors";
 import { errors } from 'celebrate'
 import cardsRouter from './routes/cards.js'
 import usersRouter from './routes/users.js'
@@ -12,6 +12,17 @@ import { requestLogger, errorLogger } from './middlewares/logger.js'
 mongoose.connect('mongodb://localhost:27017/aroundb')
 
 const app = express()
+
+app.use(cors({
+  origin: [
+    'http://web-project-around.ignorelist.com',
+    'https://web-project-around.ignorelist.com',
+    'http://localhost:3000'
+  ],
+  credentials: true
+}));
+
+
 app.use(requestLogger)
 
 app.use(express.json()) // para parsear application/json
@@ -24,15 +35,15 @@ app.use(auth)
 app.use(cardsRouter)
 app.use(usersRouter)
 
+app.use(errorLogger);
+app.use(errors());
+
 app.use('/', (req, res) => {
   res.status(404).send({
     message: 'Endpoint not found',
   })
 })
 
-app.use(errorLogger)
-
-app.use(errors())
 
 // Middleware de manejo de errores - DEBE IR AL FINAL
 app.use(errorHandler)
