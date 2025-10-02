4cbaa6130250921d7c4a37406ea1f8d4f8261557
Eduardo Cruz <ecc.informatik@icloud.com>
2025-10-02 16:06:09 -0700

    test

--- backend/app.js
+++ backend/app.js
@@ -1,43 +1,43 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import { errors } from "celebrate";
-//import helmet from "helmet";
-//import rateLimit from "express-rate-limit";
-import dotenv from "dotenv";
-import cardsRouter from "./routes/cards.js";
-import usersRouter from "./routes/users.js";
-import authRouter from "./routes/auth.js";
-import auth from "./middlewares/auth.js";
-import errorHandler from "./middlewares/errorHandler.js";
-import { requestLogger, errorLogger } from "./middlewares/logger.js";
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import { errors } from 'celebrate'
+// import helmet from "helmet";
+// import rateLimit from "express-rate-limit";
+import dotenv from 'dotenv'
+import cardsRouter from './routes/cards.js'
+import usersRouter from './routes/users.js'
+import authRouter from './routes/auth.js'
+import auth from './middlewares/auth.js'
+import errorHandler from './middlewares/errorHandler.js'
+import { requestLogger, errorLogger } from './middlewares/logger.js'
 
-dotenv.config();
+dotenv.config()
 
 const {
   PORT = 3000,
-  MONGO_URL = "mongodb://127.0.0.1:27017/aroundb",
-  ALLOWED_ORIGINS = "http://localhost:3000",
-  NODE_ENV = "development",
-} = process.env;
+  MONGO_URL = 'mongodb://127.0.0.1:27017/aroundb',
+  // ALLOWED_ORIGINS = 'http://localhost:3000',
+  NODE_ENV = 'development',
+} = process.env
 
 mongoose
   .connect(MONGO_URL)
   .then(() => {
-    if (NODE_ENV !== "test") console.log("MongoDB conectado");
+    if (NODE_ENV !== 'test') console.log('MongoDB conectado')
   })
   .catch((err) => {
-    console.error("Error conexión Mongo:", err);
-    process.exit(1);
-  });
+    console.error('Error conexión Mongo:', err)
+    process.exit(1)
+  })
 
-const app = express();
+const app = express()
 
-const allowedOrigins = new Set(
-  ALLOWED_ORIGINS.split(",")
-    .map((o) => o.trim())
-    .filter(Boolean)
-);
+// const allowedOrigins = new Set(
+//   ALLOWED_ORIGINS.split(',')
+//     .map((o) => o.trim())
+//     .filter(Boolean),
+// )
 
 // app.use(
 //   cors({
@@ -59,14 +59,14 @@ const allowedOrigins = new Set(
 // app.use(cors());
 app.use(
   cors({
-    origin: "https://web-project-around.ignorelist.com",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    //credentials: true,
-  })
-);
+    origin: 'https://web-project-around.ignorelist.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    // credentials: true,
+  }),
+)
 
-app.options("*", cors());
+app.options('*', cors())
 
 // app.use((err, req, res, next) => {
 //   if (err && err.message === "CORS_NOT_ALLOWED") {
@@ -75,7 +75,7 @@ app.options("*", cors());
 //   return next(err);
 // });
 
-//app.use(helmet());
+// app.use(helmet());
 
 // const globalLimiter = rateLimit({
 //   windowMs: 15 * 60 * 1000,
@@ -94,33 +94,33 @@ app.options("*", cors());
 
 // app.use(globalLimiter);
 
-app.use(requestLogger);
+app.use(requestLogger)
 app.use(
   express.json({
-    /*limit: "16kb",*/
-  })
-);
+    /* limit: "16kb", */
+  }),
+)
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
-app.use("/signin" /*authLimiter*/);
-app.use("/signup" /*authLimiter*/);
+app.use('/signin' /* authLimiter */)
+app.use('/signup' /* authLimiter */)
 
-app.use(authRouter);
-app.use(auth);
-app.use(cardsRouter);
-app.use(usersRouter);
+app.use(authRouter)
+app.use(auth)
+app.use(cardsRouter)
+app.use(usersRouter)
 
-app.use("/", (req, res) => {
+app.use('/', (req, res) => {
   res.status(404).send({
-    message: "Endpoint not found",
-  });
-});
+    message: 'Endpoint not found',
+  })
+})
 
-app.use(errorLogger);
-app.use(errors());
-app.use(errorHandler);
+app.use(errorLogger)
+app.use(errors())
+app.use(errorHandler)
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port  0.0.0.0:${PORT}`);
-});
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port  0.0.0.0:${PORT}`)
+})
