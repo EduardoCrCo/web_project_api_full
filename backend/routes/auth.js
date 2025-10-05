448e4229f21e66f7784f5e1f85c90ef0e3dcc3ae
Eduardo Cruz <ecc.informatik@icloud.com>
2025-10-05 00:50:26 -0700

    add change login and register to signin andsignup

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,7 +4,7 @@ import { validateRegister, validateLogin } from "../middlewares/validation.js";
 
 const router = Router();
 
-router.post("/login", validateLogin, login);
-router.post("/register", validateRegister, register);
+router.post("/signin", validateLogin, login);
+router.post("/signup", validateRegister, register);
 
 export default router;
